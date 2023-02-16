636724b45579c979890b602b3c7e4c38cd32df52
Bonifacemusyoki <124658111+Bonifacemusyoki@users.noreply.github.com>
2023-02-15 22:34:52 -0800

    added gaming functions

---
+++ game.py
@@ -0,0 +1,25 @@
+from random import randint
+
+# this program demonstrates a guessing game
+
+# get user input
+user_name=input("Enter user name:")
+
+print("hello there "+ user_name +"!")
+
+
+
+
+
+# generate a random number
+number=randint(10, 50)
+
+counter=0
+while counter < 6:
+    user_number=eval(input("Enter a number:"))
+
+
+# using a while loop check if user input is equal to generated number
+
+
+
